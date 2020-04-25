dd0f10c6c309f5b0a1b78423d8c9b045a341fa86
Anastasia Laptii <laptiyanastasia@gmail.com>
2020-04-26 00:00:45 +0300

    create dbcontext

---
+++ AlchemicShop.DAL/AlchemicDbContext/AlchemicShopContext.cs
@@ -0,0 +1,23 @@
+﻿using AlchemicShop.DAL.Entities;
+using System.Data.Entity;
+
+namespace AlchemicShop.DAL.AlchemicDbContext
+{
+    class AlchemicShopContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<OrderProduct> OrderProducts { get; set; }
+
+        static AlchemicShopContext()
+        {
+            Database.SetInitializer(new AlchemicShopInitializer());
+        }
+    }
+}
---
+++ AlchemicShop.DAL/AlchemicDbContext/AlchemicShopInitializer.cs
@@ -0,0 +1,36 @@
+﻿using AlchemicShop.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlchemicShop.DAL.AlchemicDbContext
+{
+    class AlchemicShopInitializer : DropCreateDatabaseIfModelChanges<AlchemicShopContext>
+    {
+        protected override void Seed(AlchemicShopContext dbContext)
+        {
+            var category = new Category { Name = "Poison" };
+            dbContext.Categories.Add(category);
+            dbContext.SaveChanges();
+
+            var product = new Product { Name = "Vitality", Amount = 20, Price = 20, Description = "Description", CategoryId = 1 };
+            dbContext.Products.Add(product);
+            dbContext.SaveChanges();
+
+            var order = new Order { SheduledDate = new DateTime(2020, 02, 03), Status = Status.InTransit };
+            dbContext.Orders.Add(order);
+            dbContext.SaveChanges();
+
+            var orderProduct = new OrderProduct { OrderId = 1, ProductId = 1, Amount = 10 };
+            dbContext.OrderProducts.Add(orderProduct);
+            dbContext.SaveChanges();
+
+            var user = new User { Name = "Lelouch", Login = "BritainKing", Password = "qwerty", IsAdmin = false, OrderId = 1 };
+            dbContext.Users.Add(user);
+            dbContext.SaveChanges();
+        }
+    }
+}
