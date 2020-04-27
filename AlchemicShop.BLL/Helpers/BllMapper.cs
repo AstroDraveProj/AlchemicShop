7285b5b204909bbda3ad2ef01e4b21822c895d5f
Anastasia Laptii <laptiyanastasia@gmail.com>
2020-04-27 16:26:42 +0300

    Update BllMapper.cs

--- AlchemicShop.BLL/Helpers/BllMapper.cs
+++ AlchemicShop.BLL/Helpers/BllMapper.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AlchemicShop.BLL.DTO;
+using AlchemicShop.DAL.Entities;
+using AutoMapper;
 
 namespace AlchemicShop.BLL.Helpers
 {
-    class BllMapper
+    public static class BllMapper
     {
+        public static MapperConfiguration Configure()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<User, UserDTO>();
+                cfg.CreateMap<Order, OrderDTO>();
+                cfg.CreateMap<OrderProduct, OrderProductDTO>();
+                cfg.CreateMap<Product, ProductDTO>();
+                cfg.CreateMap<Category, CategoryDTO>();
+
+                cfg.CreateMap<UserDTO, User>();
+                cfg.CreateMap<OrderDTO, Order>();
+                cfg.CreateMap<OrderProductDTO, OrderProduct>();
+                cfg.CreateMap<ProductDTO, Product>();
+                cfg.CreateMap<CategoryDTO, Category>();
+            });
+            return config;
+        }
     }
 }
