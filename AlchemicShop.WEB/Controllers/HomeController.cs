15e08215abeb17582e9b0a752751c9f3a845f082
Anastasia Laptii <laptiyanastasia@gmail.com>
2020-04-28 03:36:02 +0300

    add layout

---
+++ AlchemicShop.WEB/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace AlchemicShop.WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
