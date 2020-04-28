f891f3ab45f1070026bf5c75c86ab86c0644748a
Anastasia Laptii <laptiyanastasia@gmail.com>
2020-04-28 03:44:57 +0300

    add about page

--- AlchemicShop.WEB/Controllers/HomeController.cs
+++ AlchemicShop.WEB/Controllers/HomeController.cs
@@ -4,8 +4,12 @@ namespace AlchemicShop.WEB.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
         {
             return View();
         }
