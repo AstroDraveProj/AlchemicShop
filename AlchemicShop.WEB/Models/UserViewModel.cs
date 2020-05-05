8e5f580e96d5bf6e0f2248d9d743eeb95a680551
dravesstorm <dravesstorm@users.github.com>
2020-05-05 17:51:52 +0300

    customize actionlinks with btn style

--- AlchemicShop.WEB/Models/UserViewModel.cs
+++ AlchemicShop.WEB/Models/UserViewModel.cs
@@ -16,6 +16,9 @@ namespace AlchemicShop.WEB.Models
         public string Login { get; set; }
 
         [Required(ErrorMessage = "Please, enter password")]
+        [DataType(DataType.Password)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+
         public string Password { get; set; }
 
         public Role Role { get; set; }
