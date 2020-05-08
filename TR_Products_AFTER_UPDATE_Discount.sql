1dd11c4cf320b0e2bdd2b8bbe0ee93cf855864f0
Anastasia Laptii <laptiyanastasia@gmail.com>
2020-05-09 02:14:22 +0300

    Update TR_Products_AFTER_UPDATE_Discount.sql

--- TR_Products_AFTER_UPDATE_Discount.sql
+++ TR_Products_AFTER_UPDATE_Discount.sql
@@ -1,15 +1,9 @@
 ﻿CREATE TRIGGER [Products.TR_Product_AFTER_UPDATE_Discount]
   ON [AlchemicShopDb].[dbo].[Products]
-  AFTER UPDATE
+    FOR UPDATE
   AS
-  IF EXISTS (SELECT * 
-        FROM [AlchemicShopDb].[dbo].[Products]
-          WHERE
-          [AlchemicShopDb].[dbo].[Products].[Amount]>100)
-  BEGIN
   UPDATE [AlchemicShopDb].[dbo].[Products]
     SET [Price]=[inserted].[Price] *0.8
       FROM [inserted]
         WHERE [AlchemicShopDb].[dbo].[Products].[Id]=[inserted].[Id]
-  END
 GO
