b51244aacd71af85c3420cbeb41814c1ec1c7bf6
Anastasia Laptii <laptiyanastasia@gmail.com>
2020-05-14 07:33:14 +0300

    Create SP_Count_User_With_N_Order.sql

---
+++ SP_Count_User_With_N_Order.sql
@@ -0,0 +1,19 @@
+﻿/*Number of users with N order*/
+
+CREATE PROCEDURE [dbo].[SP_Count_User_With_N_Order]
+	@Number INT
+	AS
+		BEGIN
+			SELECT COUNT([u].[Id])
+				FROM
+				[AlchemicShopDb].[dbo].[Users] [u]
+					WHERE 
+					[u].[Id] IN
+					(SELECT [o].[UserId]
+						FROM 
+						[AlchemicShopDb].[dbo].[Orders] [o]
+							GROUP BY [o].[UserId]
+								HAVING COUNT([o].[UserId]) = @Number)
+		END
+
+--EXEC [dbo].[SP_Count_User_With_N_Order] 1
