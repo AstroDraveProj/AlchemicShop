40750d0336719c0741a7b444fc1d638b3ca21680
Anastasia Laptii <laptiyanastasia@gmail.com>
2020-05-14 07:12:36 +0300

    add SP

---
+++ SP_Count_User_With_Order.sql
@@ -0,0 +1,16 @@
+﻿/*Number of users with at least 1 order*/
+
+CREATE PROCEDURE [dbo].[SP_Count_User_With_Order]
+	AS
+		BEGIN
+			SELECT COUNT([u].[Id])
+				FROM
+				[AlchemicShopDb].[dbo].[Users] [u]
+					WHERE
+					[u].[Id] IN 
+					(SELECT DISTINCT [o].[UserId]
+						FROM
+						[AlchemicShopDb].[dbo].[Orders] [o])					
+		END
+
+--EXEC [dbo].[SP_Count_User_With_Order] 
---
+++ SP_Count_User_Without_Order.sql
@@ -0,0 +1,16 @@
+﻿/*Number of users without at least 1 order*/
+
+CREATE PROCEDURE [dbo].[SP_Count_User_Without_Order]
+	AS
+		BEGIN
+			SELECT COUNT([u].[Id])
+				FROM
+				[AlchemicShopDb].[dbo].[Users] [u]
+					WHERE
+					[u].[Id] NOT IN 
+					(SELECT DISTINCT [o].[UserId]
+						FROM
+						[AlchemicShopDb].[dbo].[Orders] [o])					
+		END
+
+--EXEC [dbo].[SP_Count_User_Without_Order] 
