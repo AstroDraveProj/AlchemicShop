fd5af30e922642becd9eab88fa2c3857c7d21c32
Anastasia Laptii <laptiyanastasia@gmail.com>
2020-05-14 09:22:08 +0300

    Create FUNCTION_ORDER_SUM.sql

---
+++ FUNCTION_ORDER_SUM.sql
@@ -0,0 +1,19 @@
+﻿CREATE FUNCTION FUNCTION_ORDER_SUM()
+    RETURNS TABLE
+        AS
+            RETURN
+                (SELECT 
+                [o].[Id],
+                SUM([p].[Price]*[op].[Amount]) as ORDERSUM
+                    FROM
+                    [AlchemicShopDb].[dbo].[OrderProducts] [op],
+                    [AlchemicShopDb].[dbo].[Orders] [o],
+                    [AlchemicShopDb].[dbo].[Products] [p]
+                        WHERE
+                        [o].[Id]=[op].[OrderId]
+                        and [p].[Id]=[op].[ProductId]
+                            GROUP BY
+                            [o].[Id]
+                           )
+
+--SELECT * FROM [dbo].[FUNCTION_ORDER_SUM]()
