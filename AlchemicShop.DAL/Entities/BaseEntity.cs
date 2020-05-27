1635c4c7a2423d756774a87cfc3525aa6e8aec73
Anastasia Laptii <laptiyanastasia@gmail.com>
2020-05-28 02:42:37 +0300

    bug fix

--- AlchemicShop.DAL/Entities/BaseEntity.cs
+++ AlchemicShop.DAL/Entities/BaseEntity.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AlchemicShop.DAL.Entities
+﻿namespace AlchemicShop.DAL.Entities
 {
     public class BaseEntity
     {
